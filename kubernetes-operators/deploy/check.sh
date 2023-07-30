5632bf3aae1379c3d4799edd7409c004afbc706b
androidraddik <a.raddik@yandex.ru>
2023-07-30 19:47:21 +0300

    scripts were deleted

--- kubernetes-operators/deploy/check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export MYSQLPOD=$(kubectl get pods -l app=mysql-instance -o jsonpath="{.items[*].metadata.name}")
-kubectl exec -it $MYSQLPOD -- mysql -potuspassword -e "select * from test;" otus-database
